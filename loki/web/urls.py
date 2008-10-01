05ccfccc441160561b1d03839af00f8b8f7ac5af
Dan Radez <dradez@redhat.com>
2008-10-01 16:23:56 -0400

    only amount worth committing today is the web stuff

--- loki/web/urls.py
+++ loki/web/urls.py
@@ -2,16 +2,27 @@ from django.conf.urls.defaults import *
 
 from views import *
 import os
+
+# mock the apache django.root option
+# http://www.example.com/loki/ 
+dr = 'loki/'
+# http://www.example.com/
+#dr=''
+
+#yui info
+yui_path = os.path.join(os.path.dirname(__file__), 'yui')
+yui_path = yui_path.replace('\\', '/')
+
 urlpatterns = patterns('',
     # serve yui files
-    (r'^yui/(?P<path>.*)$', 'django.views.static.serve', 
-         {'document_root': os.path.join(os.path.dirname(__file__), 'yui').replace('\\','/')}),
+    (r'^%syui/(?P<path>.*)$' % dr, 'django.views.static.serve',
+         {'document_root': yui_path}),
     # entry points for ajax calls
-    (r'^json/botstatus/([a-z]*)/.*', bot_status),
-    (r'^json/botreport/([a-z]*)/.*', bot_report),
+    (r'^%sjson/botstatus/([a-z]*)/.*' % dr, bot_status),
+    (r'^%sjson/botreport/([a-z]*)/.*'% dr, bot_report),
 
     # handle views
-    (r'^home/$', home),
+    (r'^%s$' % dr, home),
 
 
     # Example:
